40fe441825e65b05856f8f27494a513d61210b58
Christian Budde Christensen <budde377@cs.au.dk>
2015-03-11 13:01:45 +0100

    Removed tabsplit

--- python/core.py
+++ python/core.py
@@ -2,9 +2,11 @@ __author__ = 'budde'
 
 from abc import ABCMeta, abstractmethod
 
+
 class ArrayLibrary:
     def __init__(self):
         self.current_id = 0
+
 
     def generate_id(self, line_no, file_path, address):
         pass
@@ -30,15 +32,13 @@ class DummyHandler(Handler):
     def generate_result(self):
         return []
 
-    def handle_line(self, line):
-        line_object = line.split("\t")
+    def handle_line(self, line_object):
         if len(line_object) < 3:
             return
 
         line_type = line_object[0]
         line_number = line_object[1]
         line_file = line_object[2]
-
 
         if line_type == "array_write":
             array_ref = line_object[6]
