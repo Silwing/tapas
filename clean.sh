11353930340010b539154b2a57fd547e5516736a
Christian Budde Christensen <budde377@cs.au.dk>
2015-03-18 11:05:04 +0100

    Refactored cleanscripts

--- clean.sh
+++ clean.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
-sed '/\tphar:\/\/\//d' $1 | sed '/\t\/vagrant\/corpus\/git\/CodeIgniter\/vendor\//d'  | sed '/\t\/vagrant\/corpus\/git\/CodeIgniter\/tests\//d' | sed '/\t\/usr\/local\/bin\/phpunit/d' 
+sed -n '/^[a-z_]\+\t[0-9]\+\t\/vagrant\/corpus\/\(git\|svn\)\//p' $1 |\
+sed '/\t\/vagrant\/corpus\/svn\/trunk\/tests\//d' | \
+sed '/\t\/vagrant\/corpus\/git\/CodeIgniter\/vendor\//d' | \
+sed '/\t\/vagrant\/corpus\/git\/CodeIgniter\/tests\//d' | \
+sed '/\t\/vagrant\/corpus\/git\/joomla-cms\/libraries\/vendor\//d'  | \
+sed '/\t\/vagrant\/corpus\/git\/joomla-cms\/tests\//d' | \
+sed '/\t\/vagrant\/corpus\/git\/Part\/test\//d' | \
+sed '/\t\/vagrant\/corpus\/git\/Part\/vendor\//d'
--- clean_part.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-sed '/\tphar:\/\/\//d' $1 | sed '/\t\/vagrant\/corpus\/git\/Part\/test\//d' | sed '/\t\/vagrant\/corpus\/git\/Part\/vendor\//d' | sed '/\t\/usr\/local\/bin\/phpunit/d' 
