d0dc8265029a2d75d838dc65f6b7a044a77c74de
Randi Hillerøe <silwing@gmail.com>
2015-03-24 11:11:07 +0100

    Value handler is now done.

--- python/value_handler.py
+++ python/value_handler.py
@@ -10,36 +10,49 @@ import core
 
 class ValueHandler(core.Handler):
     def get_blacklist(self):
-        return {}
+        return {
+        }
 
     def __init__(self, library):
         super(ValueHandler, self).__init__(library)
         self.array_value_types = {}
+        self.array_types = {}
         self.last_write_id = None
-        self.multiples = {}
+        self.change_locations = {}
+        self.partFalses = [14, 49, 87, 91, 94, 165, 194, 280, 312, 331]
+        self.ciFalses = []
 
     def generate_result(self):
         multiple_locations = {}
+        multiple_maps = 0
+        multiple_lists = 0
         for id in self.array_value_types:
             types = self.array_value_types[id]
             if len(types) > 1:
-                multiple_locations[id] = "%s - %s" % (self.library.find_define(id), str(types))
+                multiple_locations[id] = "%s - %s - %s" % (self.array_types[id], self.library.find_define(id), str(types))
+                if self.array_types[id] & 4 == 4:
+                    multiple_maps += 1
+                elif self.array_types[id] & 1 == 1:
+                    multiple_lists += 1
 
-        return multiple_locations, float(len(multiple_locations)) / len(self.array_value_types), self.multiples
+        '''return multiple_locations, self.change_locations, len(self.array_types), len(multiple_locations), float(len(multiple_locations)) / len(self.array_value_types), float(multiple_maps) / len(self.array_value_types), float(multiple_lists) / len(self.array_value_types)'''
+        return [len(self.array_types), len(multiple_locations), float(len(multiple_locations)) / len(self.array_value_types), float(multiple_maps) / len(self.array_value_types), float(multiple_lists) / len(self.array_value_types)]
 
-    def add_type_to_array(self, array_id, vtype, class_name, line_number, line_file, line_type):
+    def add_type_to_array(self, array_id, vtype, class_name, line_number, line_file, line_type, current_line):
         if vtype == "object":
-            assign_type = class_name
+            assign_type = vtype
         else:
             assign_type = vtype
         if array_id not in self.array_value_types:
-            self.array_value_types[array_id] = [assign_type]
+            self.array_value_types[array_id] = {assign_type: 1}
         elif assign_type not in self.array_value_types[array_id]:
-            self.array_value_types[array_id].append(assign_type)
-            if array_id in self.multiples:
-                self.multiples[array_id].append([line_number, line_file, line_type])
+            if array_id in self.change_locations:
+                self.change_locations[array_id].append([line_number, line_file, line_type, current_line])
             else:
-                self.multiples[array_id] = [[line_number, line_file, line_type]]
+                self.change_locations[array_id] = [[line_number, line_file, line_type, current_line]]
+            self.array_value_types[array_id][assign_type] = 1
+        else:
+            self.array_value_types[array_id][assign_type] += 1
 
     def handle_line(self, line, current_line):
         if len(line) < 3:
@@ -58,15 +71,20 @@ class ValueHandler(core.Handler):
         elif line_type.startswith("assign_") and len(line) >= 14 and line[8] == "array":
             array_ref = line[12]
 
-        if array_ref is None:
-            if self.last_write_id is not None:
-                if line_type.startswith("assign_"):
-                    self.add_type_to_array(self.last_write_id, line[8], line[9], line_number, line_file, line_type)
-                self.last_write_id = None
-                return
+        if self.last_write_id is not None:
 
-        else:
+            if line_type.startswith("assign_"):
+                self.add_type_to_array(self.last_write_id, line[8], line[9], line_number, line_file, line_type, current_line)
+            self.last_write_id = None
+
+        if array_ref is not None:
+
             id = self.library.generate_id(line_number, line_file, line_type, array_ref)
+
+            if line_type.startswith("array") and (id not in self.array_types or int(line[3]) != 0):
+                self.array_types[id] = int(line[3])
+            elif line_type.startswith("assign_") and (id not in self.array_types or int(line[9]) != 0):
+                self.array_types[id] = int(line[9])
 
             if line_type in ["array_write", "array_append"]:
                 self.last_write_id = id
@@ -74,10 +92,5 @@ class ValueHandler(core.Handler):
 
             if line_type in ["array_push", "array_unshift"]:
                 for i in range(7, len(line)-1, 5):
-                    self.add_type_to_array(id, line[i], line[i+1], line_number, line_file, line_type)
+                    self.add_type_to_array(id, line[i], line[i+1], line_number, line_file, line_type, current_line)
                 return
-
-
-
-
-
