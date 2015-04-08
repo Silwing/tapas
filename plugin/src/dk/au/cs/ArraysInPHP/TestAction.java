81a028a0883d408b6304d225de050ed75aa45a6a
Christian Budde Christensen <budde377@cs.au.dk>
2015-04-08 11:19:14 +0200

    Added plugin

---
+++ plugin/src/dk/au/cs/ArraysInPHP/TestAction.java
@@ -0,0 +1,15 @@
+package dk.au.cs.ArraysInPHP;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by budde on 2/4/15.
+ */
+public class TestAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        System.out.println(e);
+    }
+
+
+}
