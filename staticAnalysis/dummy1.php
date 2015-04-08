8099e49916603794539e3d26dfed7cec24100ece
Randi Katrine Hillerøe <silwing@gmail.com>
2015-04-08 11:27:34 +0200

    Added a simple file

---
+++ staticAnalysis/dummy1.php
@@ -0,0 +1,12 @@
+<?php
+
+$monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+$monthMap = [];
+
+for($i = 1; $i <= 12; $i++) {
+	$monthMap[array_pop($monthNames)] = $i;
+}
+
+$input = $_GET["monthName"];
+
+echo $input . " is month number " . $monthMap[$input];
