8b732ace09d717c669a68d8620945deaca8db8a6
Christian Budde Christensen <christi@n-bud.de>
2015-06-01 11:24:33 +0200

    Added return test

---
+++ staticAnalysis/functionalTests/functionReturnNotLastStatementTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: budde
+ * Date: 6/1/15
+ * Time: 11:23 AM
+ */
+
+function a(){
+    return 123;
+    return "asd";
+}
+
+var_dump(a()); //Should be 123
