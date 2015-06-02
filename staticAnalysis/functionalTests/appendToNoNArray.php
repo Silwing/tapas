41f0e7678afe0c5d5c4509e923d7a39ca27bc9ef
Christian Budde Christensen <budde377@cs.au.dk>
2015-06-02 10:30:14 +0200

    Added array tests

---
+++ staticAnalysis/functionalTests/appendToNoNArray.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: budde
+ * Date: 6/2/15
+ * Time: 10:28 AM
+ */
+
+$a = 123;
+
+$a[] = 1;
+var_dump($a); //Should be either 123 or list
---
+++ staticAnalysis/functionalTests/writeToNonArray.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: budde
+ * Date: 6/2/15
+ * Time: 10:29 AM
+ */
+
+$a = 123;
+
+$a['asd'] = 123;
+var_dump($a); //Should be either 123 or map 
